968f30db2976ec925bb83f8e104e7d495e6a8e13
pacholoamit <pacholoamit.tech@gmail.com>
2024-03-22 19:09:56 +0800

    feature(slack): implement basic concept

--- src/extension.ts
+++ src/extension.ts
@@ -1,25 +1,43 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
-import * as vscode from 'vscode';
+import * as vscode from "vscode";
+import { WebClient } from "@slack/web-api";
 
-// This method is called when your extension is activated
-// Your extension is activated the very first time the command is executed
-export function activate(context: vscode.ExtensionContext) {
+const token = "xoxp-6842186170917-6857766016257-6867969372448-f585b7f89af51423ad6b5a693a007884";
+const client = new WebClient(token);
 
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "slack-presence" is now active!');
+const statusBarIcon: vscode.StatusBarItem = vscode.window.createStatusBarItem(vscode.StatusBarAlignment.Left);
+statusBarIcon.text = "$(pulse) Connecting to Slack...";
+statusBarIcon.show();
 
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('slack-presence.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from slack-presence!');
-	});
+const enable = async () => {
+  await client.users.profile
+    .set({
+      profile: {
+        status_text: "Working on VS Code",
+        status_emoji: ":technologist:",
+        status_expiration: 0,
+      },
+    })
+    .then((_) => {
+      statusBarIcon.text = "$(globe) Connected to Slack";
+      statusBarIcon.tooltip = "Connected to Slack";
+      statusBarIcon.show();
+    })
+    .catch((error) => {
+      statusBarIcon.text = "$(alert) Error connecting to Slack";
+      statusBarIcon.tooltip = "Error connecting to Slack";
+      statusBarIcon.show();
+      console.error(error);
+    });
 
-	context.subscriptions.push(disposable);
+  vscode.window.showInformationMessage("Slack Presence is now active!");
+};
+
+export async function activate(context: vscode.ExtensionContext) {
+  const enabler = vscode.commands.registerCommand("slack-presence.enable", async () => {
+    await enable();
+  });
+
+  context.subscriptions.push(enabler);
 }
 
 // This method is called when your extension is deactivated
